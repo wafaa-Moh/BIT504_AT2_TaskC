c8f9439c6bd2390d11911c10116c99be1e01c91f
wafaa-Moh <wafaashaban@gmail.com>
2022-08-27 15:54:01 +1200

    creating a main class including the main method to run the game

---
+++ PingPongGame/src/Pong.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+   
+   public class Pong extends JFrame {
+       
+       private final static String WINDOW_TITLE = "Pong";
+       private final static int WINDOW_WIDTH = 800;
+       private final static int WINDOW_HEIGHT = 600;
+       
+       public Pong() {
+              setTitle(WINDOW_TITLE);
+          setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+              setResizable(false);
+              add(new PongPanel());
+              setVisible(true);
+              setDefaultCloseOperation(EXIT_ON_CLOSE);
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	           public void run() {
+	                  new Pong();
+	           }
+	         });
+	}
+
+}
