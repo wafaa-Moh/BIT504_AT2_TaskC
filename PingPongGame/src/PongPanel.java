4c1c56f2e7a73086af4a7ca357230c8428c84b0a
wafaa-Moh <wafaashaban@gmail.com>
2022-08-27 21:08:18 +1200

    added Update method for updating Game states
    added CreateObjects method for creating ball and the paddles
    
    updated paintComponent to paint the ball and the paddles to the game
    
    call update and repaint methods in actionPerformed

--- PingPongGame/src/PongPanel.java
+++ PingPongGame/src/PongPanel.java
@@ -15,13 +15,44 @@ import javax.swing.JPanel;
 public class PongPanel extends JPanel implements ActionListener,KeyListener{
 	 private final static Color BACKGROUND_COLOUR = Color.BLACK;
      private final static int TIMER_DELAY = 5;
+     
+     GameState gameState = GameState.INITIALISING;
+     
+     
 
+     Ball ball;
+     Paddle paddle1, paddle2;
 	public PongPanel() {
 		// TODO Auto-generated constructor stub
 		setBackground(BACKGROUND_COLOUR); //setting background color
         Timer timer = new Timer(TIMER_DELAY,this);
         timer.start();
+        addKeyListener(this);
+        setFocusable(true);
 	}
+	
+	 private void update() {
+         switch(gameState) {
+             case INITIALISING: {
+          	   createObjects();
+                 gameState = GameState.PLAYING;
+                
+                 break;
+             }
+             case PLAYING: {
+                 break;
+             }
+             case GAMEOVER: {
+                 break;
+             }
+         }
+     }
+	
+	public void createObjects() {
+        ball = new Ball(getWidth(), getHeight());
+        paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+        paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+    }
 	
 	 private void paintDottedLine(Graphics g) {
          Graphics2D g2d = (Graphics2D) g.create();
@@ -31,6 +62,11 @@ public class PongPanel extends JPanel implements ActionListener,KeyListener{
              g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
              g2d.dispose();
       }
+	 
+	 private void paintSprite(Graphics g, Sprite sprite) {
+         g.setColor(sprite.getColour());
+         g.fillRect(sprite.getXPosition(), sprite.getYPosition(), sprite.getWidth(), sprite.getHeight());
+     }
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -54,11 +90,20 @@ public class PongPanel extends JPanel implements ActionListener,KeyListener{
 	 public void paintComponent(Graphics g) {
 	      super.paintComponent(g);
 	      paintDottedLine(g);
+	      if(gameState != GameState.INITIALISING) {
+              paintSprite(g, ball);
+              paintSprite(g, paddle1);
+              paintSprite(g, paddle2);
+              
+          }
+
 	 }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		update();
+        repaint();
 		
 	}
 
